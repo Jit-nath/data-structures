708f6d587d88b3fd7d74031c0d5167fa4d3a1323
Jit Debnath <jit.nathdeb@gmail.com>
2024-04-22 10:20:06 +0530

    added code from previous repo

---
+++ string-operation/library_functions.c
@@ -0,0 +1,12 @@
+// #include<string.h> // including this is necessary 
+// char name[200] , new_name[200] ; 
+// int length , compare;
+// printf("Enter something: ");
+// fgets(name,sizeof(name),stdin);
+// printf("%s",name);
+// length = strlen(name); // to get the length of the string
+// compare = strcmp(name,name); // to compare 2 strings
+// new_name[200]; strcpy(new_name,name); // to copy a string into another array
+//  strcat(name,new_name); // to add or merge two different string
+// strrev(new_name); // to reverse a string 
+      
